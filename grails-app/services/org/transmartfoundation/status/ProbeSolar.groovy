359159607bae72cad13046bb69e592fdfd159304
Terry-Weymouth <weymouth@umich.edu>
2016-01-19 16:32:24 -0500

    A stub for the interface to probe solr for status

---
+++ grails-app/services/org/transmartfoundation/status/ProbeSolar.groovy
@@ -0,0 +1,13 @@
+package org.transmartfoundation.status
+
+/**
+ * Created by weymouth on 1/19/16.
+ */
+class ProbeSolr {
+
+    boolean exists(){
+        println "the actual ProbeSolr.exists() got called! "
+        return true
+    }
+
+}
