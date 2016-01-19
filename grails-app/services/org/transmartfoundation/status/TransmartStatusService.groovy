c8cd41281b575b7798e1fe73cf40229f42f4887c
Terry-Weymouth <weymouth@umich.edu>
2016-01-19 16:33:12 -0500

    Added initial sketch of TransmartStatsService: probe solr for status

--- grails-app/services/org/transmartfoundation/status/TransmartStatusService.groovy
+++ grails-app/services/org/transmartfoundation/status/TransmartStatusService.groovy
@@ -4,4 +4,17 @@ package org.transmartfoundation.status
  * Created by weymouth on 1/19/16.
  */
 class TransmartStatusService {
+
+    ProbeSolr solrProbe
+
+    def TransmartStatusService(){
+        solrProbe = new ProbeSolr()
+    }
+
+    def boolean solrExists() {
+        println(solrProbe)
+        boolean retValue = solrProbe.exists()
+        println(retValue)
+        return retValue
+    }
 }
--- test/integration/org/transmartfoundation/status/StatusServiceTests.groovy
+++ test/integration/org/transmartfoundation/status/StatusServiceTests.groovy
@@ -1,19 +1,33 @@
 package org.transmartfoundation.status
 
+import org.gmock.WithGMock
 import org.junit.Test
 import static org.hamcrest.MatcherAssert.assertThat
 
 /**
  * Created by weymouth on 1/19/16.
  */
+@WithGMock
 class StatusServiceTests {
 
     def transmartStatusService
 
     @Test
     void testThatStatusServiceDoesExists() {
-        println transmartStatusService
-        assertThat "test: does the status server exist?" , transmartStatusService != null
+        assertThat "test: the status server exist?" , transmartStatusService != null
     }
 
+    @Test
+    void testThatSolrDoesNotExistsWithMockProbe() {
+        ProbeSolr probe = mock(ProbeSolr)
+        probe.exists().returns false;
+
+        transmartStatusService.solrProbe = probe
+        def retValue
+        play {
+            retValue = transmartStatusService.solrExists()
+            println("Returned value = " + retValue)
+        }
+        assertThat "test: mocked solr probe returns false" , !retValue
+    }
 }
